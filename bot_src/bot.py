43c4f3f76586b3fcab912bab09195d79c509c81d
KiLJ4EdeN <parsa592323@gmail.com>
2022-01-07 00:55:15 +0330

    database for bot, adduser, add, deduct, reset cmds

--- bot_src/bot.py
+++ bot_src/bot.py
@@ -13,6 +13,9 @@ from dotenv import load_dotenv
 # IMPORT COMMANDS FROM THE DISCORD.EXT MODULE.
 from discord.ext import commands
 
+# DATABASE
+from database import SQLiteDatabase
+
 # LOADS THE .ENV FILE THAT RESIDES ON THE SAME LEVEL AS THE SCRIPT.
 load_dotenv()
 
@@ -21,6 +24,9 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 # CREATES A NEW BOT OBJECT WITH A SPECIFIED PREFIX. IT CAN BE WHATEVER YOU WANT IT TO BE.
 bot = commands.Bot(command_prefix=".")
+
+# Initiate the database
+db = SQLiteDatabase(initiate=False)
 
 
 # ON_MESSAGE() EVENT LISTENER. NOTICE IT IS USING @BOT.EVENT AS OPPOSED TO @BOT.COMMAND().
@@ -47,6 +53,84 @@ async def ping(ctx):
     await ctx.channel.send("pong")
 
 
+# COMMAND .adduser, adds a user to the database using their nickname
+@bot.command(
+    help="usage: .adduser Insane-Kazzak",
+    brief="adds a user to the database with 0 balance"
+)
+async def adduser(ctx, name: str):
+    name = name.lower()
+    try:
+        db.fetch_user_balance(user_name=name)
+        await ctx.author.send(f"user: {name} is already registered! skipping...")
+    except AttributeError:
+        db.insert_users(user_list=[{'name': name, 'balance': 0},
+                                   ])
+        await ctx.author.send(f"user: {name} successfully created!")
+
+
+# COMMAND .add, add balance for a user
+@bot.command(
+    help="usage: .add Insane-Kazzak 1000",
+    brief="adds balance for a user"
+)
+async def add(ctx, name: str, value: float):
+    name = name.lower()
+    try:
+        bal = db.fetch_user_balance(user_name=name)
+        db.update_user_balance(user_name=name, value=bal+value)
+        await ctx.author.send(f"user: balance increased from {bal} to {bal+value}")
+    except AttributeError:
+        await ctx.author.send(f"user: {name} does not exist!")
+
+
+# COMMAND .deduct, lower balance of a user
+@bot.command(
+    help="usage: .deduct Insane-Kazzak 1000",
+    brief="lower balance of a user"
+)
+async def deduct(ctx, name: str, value: float):
+    name = name.lower()
+    try:
+        bal = db.fetch_user_balance(user_name=name)
+        db.update_user_balance(user_name=name, value=bal-value)
+        await ctx.author.send(f"user: balance decreased from {bal} to {bal-value}")
+    except AttributeError:
+        await ctx.author.send(f"user: {name} does not exist!")
+
+
+# COMMAND .reset, resets balance of a user
+@bot.command(
+    help="usage: .reset Insane-Kazzak",
+    brief="resets balance of a user"
+)
+async def reset(ctx, name: str):
+    name = name.lower()
+    try:
+        db.fetch_user_balance(user_name=name)
+        db.update_user_balance(user_name=name, value=0)
+        await ctx.author.send(f"user: balance reset to 0")
+    except AttributeError:
+        await ctx.author.send(f"user: {name} does not exist!")
+
+
+# COMMAND .B. THIS DMs THE USER THEIR BALANCE
+@bot.command(
+    # ADDS THIS VALUE TO THE .HELP PRINT MESSAGE.
+    help="DMs the balance",
+    # ADDS THIS VALUE TO THE .HELP MESSAGE.
+    brief="DMs the balance"
+)
+async def b(ctx):
+    name = ctx.author.nick
+    name = name.lower()
+    try:
+        bal = db.fetch_user_balance(user_name=name)
+        await ctx.author.send(f"your balance is: {bal}")
+    except AttributeError:
+        await ctx.author.send(f"user: {name} does not exist!")
+
+
 # COMMAND .PRINT. THIS TAKES AN IN A LIST OF ARGUMENTS FROM THE USER AND SIMPLY PRINTS THE VALUES BACK TO THE CHANNEL.
 @bot.command(
     # ADDS THIS VALUE TO THE .HELP PRINT MESSAGE.
@@ -63,19 +147,6 @@ async def print(ctx, *args):
 
     # SENDS A MESSAGE TO THE CHANNEL USING THE CONTEXT OBJECT.
     await ctx.channel.send(response)
-
-
-# COMMAND .B. THIS DMs THE USER THEIR BALANCE
-@bot.command(
-    # ADDS THIS VALUE TO THE .HELP PRINT MESSAGE.
-    help="DMs the balance",
-    # ADDS THIS VALUE TO THE .HELP MESSAGE.
-    brief="DMs the balance"
-)
-async def b(ctx):
-    response = f"Your Balance is ... \n{ctx.author}"
-    # SENDS A MESSAGE TO THE CHANNEL USING THE CONTEXT OBJECT.
-    await ctx.author.send(response)
 
 
 # COMMAND .CUT. calcs cut
@@ -111,6 +182,7 @@ async def cut(ctx, *args):
         except ValueError:
             response = "arguments to .cut ! MUST be numbers"
             await ctx.author.send(response)
+
 
 # COMMAND .divide. divide a number by another
 @bot.command(
---
+++ bot_src/database.py
@@ -0,0 +1,105 @@
+"""
+Database for the bot
+"""
+from sqlalchemy import create_engine
+from sqlalchemy import Float, String, MetaData, Table, Integer, Column
+
+
+class SQLiteDatabase(object):
+    """
+    sqlite db utility to ease working with the bot
+    """
+    def __init__(self, db_path='sqlite:///bot.db', initiate=False):
+        self.engine = create_engine(db_path, echo=True)
+        self.conn = self.engine.connect()
+        self.meta = MetaData()
+        self.discord_users = Table(
+            'discord_users', self.meta,
+            Column('id', Integer, primary_key=True),
+            Column('name', String),
+            Column('balance', Float(precision=10)),
+        )
+
+        if initiate:
+            self.initiate()
+
+    def initiate(self):
+        """
+        create the table using this command:
+        CREATE TABLE discord_users (
+        id INTEGER NOT NULL,
+        name VARCHAR,
+        balance FLOAT,
+        PRIMARY KEY (id)
+        )
+        COMMIT
+        :return: None
+        """
+        self.meta.create_all(self.engine)
+
+    def insert_users(self, user_list: list):
+        """
+        create users using this command:
+        INSERT INTO discord_users (name, balance) VALUES (?, ?)
+        (('KiLJ4EdeN', 1000.0), ('Insane', 1111.0), ('Rippah', 0.0), ('mr np', 40000000.0))
+        COMMIT
+        :return: None
+        """
+        # TODO: check names here of there are duplicates throw an error
+        self.conn.execute(self.discord_users.insert(),
+                          user_list)
+
+    def fetch_all(self):
+        """
+        SELECT discord_users.id, discord_users.name, discord_users.balance
+        FROM discord_users
+        :return: result of the query
+        """
+        cmd = self.discord_users.select()
+        result = self.conn.execute(cmd)
+        return result
+
+    def fetch_user_balance(self, user_name):
+        """
+        SELECT discord_users.id, discord_users.name, discord_users.balance
+        FROM discord_users
+        WHERE discord_users.name = ?
+        ('KiLJ4EdeN',)
+        :return: user balance
+        """
+        cmd = self.discord_users.select().where(self.discord_users.c.name == user_name)
+        result = self.conn.execute(cmd)
+        return result.first().balance
+
+    def update_user_balance(self, user_name, value: float):
+        """
+        SELECT discord_users.id, discord_users.name, discord_users.balance
+        FROM discord_users
+        WHERE discord_users.name = ?
+        ('KiLJ4EdeN',)
+        :return: None
+        """
+        cmd = self.discord_users.update().where(self.discord_users.c.name == user_name).values(balance=value)
+        self.conn.execute(cmd)
+        return
+
+
+if __name__ == '__main__':
+    db = SQLiteDatabase(initiate=True)
+    # insertion
+    db.insert_users(user_list=[{'name':'KiLJ4EdeN', 'balance': 1000},
+                               {'name':'Insane', 'balance': 1111},
+                               {'name':'Rippah', 'balance': 0},
+                               {'name':'mr np', 'balance': 40000000},
+                               ])
+    # fetching all
+    # res = db.fetch_all()
+    # for row in res:
+    #     print(row)
+    # fetchone
+    bal = db.fetch_user_balance(user_name='KiLJ4EdeN')
+    print(bal)
+    # updates
+    db.update_user_balance(user_name='KiLJ4EdeN', value=0)
+    bal = db.fetch_user_balance(user_name='KiLJ4EdeN')
+    print(bal)
